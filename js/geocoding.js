8392e9af13f00796f56c5007161504cba6ba3840
Andrew Leonard <AndrewCLeonard@fastmail.com>
2022-08-25 21:14:42 -0700

    API key fixed with .gitignore

---
+++ js/geocoding.js
@@ -0,0 +1,30 @@
+import openWeatherKey from "./keys";
+
+const textSearchEl = document.getElementById("citySearch");
+const formBtnEl = document.getElementById("formBtn");
+
+// get value of text box
+const citySearched = "";
+
+function getTextValue(e) {
+	e.preventDefault;
+	const citySearched = textSearchEl.value;
+	console.log(citySearched);
+	// handle search response or blank queries
+	citySearched ? runGeoCodingAPI(citySearched) : alertMessage();
+}
+
+function runGeoCodingAPI(citySearched) {
+	const apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${citySearched}&limit=5&appid=${openWeatherKey}`;
+	fetch(apiURL) //
+		.then(function (response) {
+			if (response.ok) {
+				console.log(response);
+				console.log(response.json());
+			}
+		});
+}
+// TO-DO
+function alertMessage() {}
+
+formBtnEl.addEventListener("click", getTextValue, false);
