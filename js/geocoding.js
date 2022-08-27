a779a9b5c81a3139fea42e3e2b5da6753a12a8f3
Andrew Leonard <AndrewCLeonard@fastmail.com>
2022-08-26 17:07:28 -0700

    WIP understanding fetch api

--- js/geocoding.js
+++ js/geocoding.js
@@ -3,6 +3,8 @@ import openWeatherKey from "./keys.js";
 const textSearchEl = document.getElementById("citySearch");
 const formBtnEl = document.getElementById("formBtn");
 
+let searchResult = [];
+
 // get value of text box
 const citySearched = "";
 
@@ -15,14 +17,32 @@ function getTextValue(e) {
 }
 
 function runGeoCodingAPI(citySearched) {
-	const apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${citySearched}&limit=5&appid=${openWeatherKey}`;
+	const apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${citySearched}&limit=5&appid=${apiKeys.googleMapsKey}`;
 	fetch(apiURL) //
-		.then(function (response) {
-			if (response.ok) {
-				console.table(response.json());
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error(`HTTP error, status = ${response.status}`);
+				// console.table(response.json());
 			}
+			return response.json();
 		});
+	// .then((data) => {
+	// 	for (const name of data.name) {
+	// 		const listItem = document.createElement("li");
+
+	// 		const nameElement = document.createElement("strong");
+	// 		nameElement.textContent =
+	// 	}
+	// })
 }
+
+fetch("testData.json") //
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error(`HTTP error, status = ${response.status}`);
+		}
+	});
+
 // TO-DO
 function alertMessage() {}
 
