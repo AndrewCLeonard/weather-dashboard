67e1d74dbfcaaee012274eb2f888273b58129529
Andrew Leonard <AndrewCLeonard@fastmail.com>
2022-09-05 22:32:21 -0700

    understanding UTC dates

---
+++ js/sampleForecastObject.js
@@ -0,0 +1,41 @@
+const formattedForcastData = {
+	"2022-09-03": {
+		temp_min: 75.34,
+		temp_max: 76.23,
+		humidity_min: 45,
+		humidity_max: 90,
+	},
+
+	"2022-09-04": {
+		temp_min: 75.34,
+		temp_max: 76.23,
+		humidity_min: 45,
+		humidity_max: 90,
+	},
+	"2022-09-05": {
+		temp_min: 75.34,
+		temp_max: 76.23,
+		humidity_min: 45,
+		humidity_max: 90,
+	},
+	"2022-09-06": {
+		temp_min: 75.34,
+		temp_max: 76.23,
+		humidity_min: 45,
+		humidity_max: 90,
+	},
+	"2022-09-07": {
+		temp_min: 75.34,
+		temp_max: 76.23,
+		humidity_min: 45,
+		humidity_max: 90,
+	},
+	"2022-09-08": {
+		temp_min: 75.34,
+		temp_max: 76.23,
+		humidity_min: 45,
+		humidity_max: 90,
+	},
+};
+
+export default formattedForcastData;
--- js/test.js
+++ js/test.js
@@ -1,4 +1,17 @@
 import json from "./testForecastData.js";
+import formattedForcastData from "./sampleForecastObject.js";
+
+// console.log(formattedForcastData["2022-09-03"].temp_min);
+
+let date = new Date(json.list[0].dt * 1000);
+console.log(date);
+console.log(date.getDay());
+console.log(date.getMonth());
+console.log(date.getDate());
+const d = new Date(date);
+console.log(d);
+
+console.log(Date());
 
 const bodyEl = document.getElementById("bodyEl");
 
@@ -29,16 +42,15 @@ for (let i = 0; i < json.list.length; i++) {
 
 // create array from all unique "dt_txt" values (date and time)
 // cons: need to split on space to find date, more straightforward to use UTC code?
-let dateTextArray = [];
-for (let i = 0; i < json.list.length; i++) {
-	// extract dates only, removing the times
-	let dateText = json.list[i].dt_txt.slice(0, 10);
-	// convert dates into proper format
-	dateText = dateFormatConversion(dateText);
-	if (dateTextArray.indexOf(dateText) === -1) {
-		dateTextArray.push(dateText);
-	}
-}
+// for (let i = 0; i < json.list.length; i++) {
+// 	// extract dates only, removing the times
+// 	let dateText = json.list[i].dt_txt.slice(0, 10);
+// 	// convert dates into proper format
+// 	dateText = dateFormatConversion(dateText);
+// 	if (dateTextArray.indexOf(dateText) === -1) {
+// 		dateTextArray.push(dateText);
+// 	}
+// }
 
 /**
  * for...of version
@@ -51,11 +63,42 @@ for (const date of json.list) {
  * for...each version
  */
 
+let dateTextArray = [];
+
 json.list.forEach((date) => {
-	console.table(date.dt_txt);
+	// extract dates only, removing the times
+	let dateText = date.dt_txt.slice(0, 10);
+	// convert dates into proper format
+	if (dateTextArray.indexOf(dateText) === -1) {
+		dateTextArray.push(dateText);
+	}
 });
 
-const testDate = "2022-09-04";
+// find min/max temps of that day
+let tempMinArray = [];
+let minTemp;
+
+const reformattedJson = [];
+
+// set dateText in original fetch request for baseline
+
+// format dateTextArray to "mm/dd/yyyy"
+json.list.map((date) => {
+	let dateText = date.dt_txt.slice(0, 10);
+	if (reformattedJson.indexOf(dateText) === -1) {
+		reformattedJson.push({ [dateText]: date.main.temp_min });
+	}
+	Math.min;
+});
+
+// console.table(reformattedJson);
+
+dateTextArray.forEach((date) => {
+	json.list.forEach((minTemp) => {
+		let dateText = minTemp.dt_txt.slice(0, 10);
+		// dateText === date ? console.log(true) : console.log(false);
+	});
+});
 
 function dateFormatConversion(date) {
 	const year = date.slice(0, 4);
@@ -63,5 +106,3 @@ function dateFormatConversion(date) {
 	const day = date.slice(8, 10);
 	return `${month}/${day}/${year}`;
 }
-
-dateFormatConversion(testDate);
